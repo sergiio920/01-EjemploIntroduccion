37ab74b56b43b60e4de1b0c40395e00303f2d9b6
sergiomoron <sergio.moronvidal@cieep.com>
2023-09-15 12:23:23 +0200

    Ejemplo 1 de android completado

--- app/src/main/java/sergio/moron/a01_ejemplointroduccion/MainActivity.java
+++ app/src/main/java/sergio/moron/a01_ejemplointroduccion/MainActivity.java
@@ -3,12 +3,39 @@ package sergio.moron.a01_ejemplointroduccion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //VARIABLES DE LA INTERFAZ
+    private TextView txtContador;
+    private Button btnPulsar;
+
+    //VARIABLES PARA LA LÓGICA
+    private int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO IRÁ AQUÍ DETRÁS
+        inicializar();
+
+        btnPulsar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                txtContador.setText("Pulsado "+contador+" veces");
+            }
+        });
+    }
+
+    private void inicializar() {
+        contador = 0;
+
+        txtContador = findViewById(R.id.txtContadorMain);
+        btnPulsar =  findViewById(R.id.btnPulsarMain);
     }
 }
